f3070d6dba3597fedc24bc78907b56ee84ecdf0c
lihuan <lihuan@Ubuntu.myguest.virtualbox.org>
2025-11-15 22:17:27 -0700

    Add sensor code

--- tmpcontrol/tmpcontrol.c
+++ tmpcontrol/tmpcontrol.c
@@ -18,13 +18,23 @@
 #include <errno.h>
 
 #define ONE_BYTE 1
+#define CONTINUOUS 0xE2 // Continuous measurement, 9-bit resolution
+#define NINE_BIT   0x80
+
 const char btn_dev[] = "/dev/button";
+const char sensor_dev[] = "/dev/ds1722";
+
 int main ()
 {
     int fd;
 	int btn_value = 0;
     ssize_t ret_byte;
 	char btn_buf[ONE_BYTE];
+	char sensor_buf[2];
+	char sensor_conf[ONE_BYTE] = CONTINUOUS;
+	float temp_value = 0.0;
+	int temp_int = 0;
+	float temp_decimal = 0.0;
 	
     fd = open(btn_dev, O_RDONLY);	
 	if (fd == -1){
@@ -47,6 +57,38 @@ int main ()
 	printf("Button value: %d\n", btn_value);
 	
 	close(fd);
+	
+	fd = open(sensor_dev, O_RDWR);
+	if (fd == -1){
+		perror("open");
+		syslog(LOG_ERR, "open");
+		return -1;
+	}
+	
+	// write sensor configuration
+	ret_byte = write(fd, sensor_conf, sizeof(sensor_conf));
+	if (ret_byte != 1){
+	    perror("write");
+		syslog(LOG_ERR, "write");
+		return -1;
+	}
+	
+	// read temperature value
+	ret_byte = read(fd, sensor_buf, sizeof(sensor_buf));
+	if (ret_byte != 2){
+		perror("read");
+		syslog(LOG_ERR, "read");
+		return -1;
+	}
+	
+	temp_int = (int)sensor_buf[0];
+	if (sensor_buf[0] == NINE_BIT)
+		temp_decimal = 0.5;
+	else
+		temp_decimal = 0.0;
+	
+	temp_value = (float)temp_int + temp_decimal;
+	printf("Temperature value: %f\n", temp_value);
 	
 	return 0;
 }
