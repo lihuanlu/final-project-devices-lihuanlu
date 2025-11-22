b201640145a35dc6b1a3cfff8180d11e2e798f1e
lihuan <lihuan@Ubuntu.myguest.virtualbox.org>
2025-11-22 16:22:51 -0700

    Edit turn off debug in device.h

--- button/device.h
+++ button/device.h
@@ -10,7 +10,7 @@
 
 //#include "aesd-circular-buffer.h" // To use the circular buffer structure
 
-#define AESD_DEBUG 1  //Remove comment on this line to enable debug
+//#define AESD_DEBUG 1  //Remove comment on this line to enable debug
 
 #undef PDEBUG             /* undef it, just in case */
 #ifdef AESD_DEBUG
